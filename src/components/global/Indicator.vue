15ec6fb46816c823ad2908f29d1dcb537a1d78dc
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-06-16 08:18:50 +0200

    development: opzet van een loading of saving indicator

---
+++ src/components/global/Indicator.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div class="vld-parent">
+    <loading :active="isLoading"
+             :is-full-page="fullPage"
+             :color="color"
+             :loader="loader"
+             :background-color="bgColor"
+             :height="height"
+             :width="width"
+             transition="fade"
+    >
+    </loading>
+  </div>
+</template>
+
+<script>
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
+import Loading from 'vue3-loading-overlay';
+
+
+export default {
+  components: {
+    Loading
+  },
+  name: "Indicator",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    },
+    fullPage: {
+      type: Boolean,
+      default: true
+    },
+    canCancel: {
+      type: Boolean,
+      default: false
+    },
+    useSlot: {
+      type: Boolean,
+      default: false
+    },
+    loader: {
+      type: String,
+      default: "spinner",
+    },
+    color: {
+      type: String,
+      default: "#7b8f1c",
+    },
+    bgColor: {
+      type: String,
+      default: "#ffffff",
+    },
+    height: {
+      type: Number,
+      default: 128,
+    },
+    width: {
+      type: Number,
+      default: 128,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
