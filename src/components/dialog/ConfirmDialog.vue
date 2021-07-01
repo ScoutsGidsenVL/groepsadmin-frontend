c09c749d317dd4a4720234fada22a74fa79e3c0a
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-07-01 09:26:50 +0200

    dev: bevestigingsdialoog opgezet

---
+++ src/components/dialog/ConfirmDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <Dialog header="Bevestigen" v-model:visible="openDialog" :style="{width: '550px'}" :modal="true" :closable="false">
+    <div class="confirmation-content">
+      <span v-html="message"></span>
+    </div>
+    <template #footer>
+      <Button label="Nee" icon="pi pi-check" @click="annuleer" class="p-button-text cancel-button" autofocus />
+      <Button label="Ja" icon="pi pi-times" @click="bevestig" class="p-button-text accept-button"/>
+    </template>
+  </Dialog>
+</template>
+
+<script>
+
+export default {
+  name: "ConfirmDialog",
+  props: {
+    message: {
+      type: String,
+    },
+    header: {
+      type: String,
+    },
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    bevestig() {
+      this.$emit('confirm')
+    },
+
+    annuleer() {
+      this.$emit('cancel')
+    }
+  },
+  computed: {
+    openDialog() {
+      return this.dialogVisible;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
