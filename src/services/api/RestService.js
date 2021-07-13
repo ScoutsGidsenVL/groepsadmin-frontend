b07923aa968b79e7c2c3255b18f560eb8a4e01f8
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-07-13 12:43:37 +0200

    dev: toevoegen van een config.json

--- src/services/api/RestService.js
+++ src/services/api/RestService.js
@@ -2,126 +2,141 @@ import apiClient from "@/services/api/ApiClient";
 import apiClientCsv from "@/services/api/ApiClientCsv";
 import apiClientPdf from "@/services/api/ApiClientPdf";
 import apiClientMail from "@/services/api/ApiClientMail";
+import apiData from "../../../public/config.json";
 
-const baseUrl = "/groepsadmin/rest-ga/";
+const apiConfig = apiData.api.baseUrl;
+let baseUrl = "";
+if (process.env.NODE_ENV === "production") {
+  baseUrl = apiConfig + "/groepsadmin/rest-ga/";
+} else {
+  baseUrl = "/groepsadmin/rest-ga/";
+}
 
 export default {
+  getWebsites() {
+    return apiClient().get(baseUrl + "website");
+  },
 
-    getWebsites() {
-        return apiClient().get(baseUrl + "website");
-    },
+  zoeken(zoekTerm) {
+    return apiClient().get(baseUrl + "zoeken", { params: { query: zoekTerm } });
+  },
 
-    zoeken(zoekTerm) {
-        return apiClient().get(baseUrl + "zoeken", {params: {query: zoekTerm}});
-    },
+  async getLid(id) {
+    return apiClient().get(baseUrl + "lid/" + id);
+  },
 
-    async getLid(id) {
-        return apiClient().get(baseUrl + "lid/" + id,);
-    },
+  zoekGemeente(zoekTerm) {
+    return apiClient().get(baseUrl + "gis/gemeente", {
+      params: { term: zoekTerm },
+    });
+  },
 
-    zoekGemeente(zoekTerm) {
-        return apiClient().get(baseUrl + "gis/gemeente", {params: {term: zoekTerm}});
-    },
+  zoekStraat(zoekTerm, postcode) {
+    return apiClient().get(baseUrl + "gis/straat", {
+      params: { term: zoekTerm, postcode: postcode },
+    });
+  },
 
-    zoekStraat(zoekTerm, postcode) {
-        return apiClient().get(baseUrl + "gis/straat", {params: {term: zoekTerm, postcode: postcode}});
-    },
+  getGroepen() {
+    return apiClient().get(baseUrl + "groep");
+  },
 
-    getGroepen() {
-        return apiClient().get(baseUrl + "groep");
-    },
+  getGroepOpId(id) {
+    return apiClient().get(baseUrl + "groep/" + id);
+  },
 
-    getGroepOpId(id) {
-        return apiClient().get(baseUrl + "groep/" + id,);
-    },
+  getFuncties() {
+    return apiClient().get(baseUrl + "functie");
+  },
 
-    getFuncties() {
-        return apiClient().get(baseUrl + "functie");
-    },
+  getLeden(offset) {
+    return apiClient().get(baseUrl + "ledenlijst?offset=" + offset);
+  },
 
-    getLeden(offset) {
-        return apiClient().get(baseUrl + "ledenlijst?offset=" + offset)
-    },
+  getLedenCsv(offset, lidIds) {
+    return apiClientCsv().post(baseUrl + "ledenlijst?offset=" + offset, lidIds);
+  },
 
-    getLedenCsv(offset, lidIds) {
-        return apiClientCsv().post(baseUrl + "ledenlijst?offset=" + offset, lidIds)
-    },
+  getLedenPdf(offset, lidIds) {
+    return apiClientPdf().post(baseUrl + "ledenlijst?offset=" + offset, lidIds);
+  },
 
-    getLedenPdf(offset, lidIds) {
-        return apiClientPdf().post(baseUrl + "ledenlijst?offset=" + offset, lidIds)
-    },
+  getLedenSteekkaartPdf(lidIds) {
+    return apiClientPdf().post(baseUrl + "ledenlijst/steekkaart", lidIds);
+  },
 
-    getLedenSteekkaartPdf(lidIds) {
-        return apiClientPdf().post(baseUrl + "ledenlijst/steekkaart", lidIds)
-    },
+  getHuidigeFilter() {
+    return apiClient().get(baseUrl + "ledenlijst/filter/huidige");
+  },
 
-    getHuidigeFilter() {
-        return apiClient().get(baseUrl + "ledenlijst/filter/huidige")
-    },
+  getFilterOpId(id) {
+    return apiClient().get(baseUrl + "ledenlijst/filter/" + id);
+  },
 
-    getFilterOpId(id){
-        return apiClient().get(baseUrl + "ledenlijst/filter/" + id)
-    },
+  // todo vereenvoudigen dmv een parameter icm mail
+  getSjablonen(type) {
+    return apiClient().get(baseUrl + "sjabloon/" + type);
+  },
 
-    // todo vereenvoudigen dmv een parameter icm mail
-    getSjablonen(type) {
-        return apiClient().get(baseUrl + "sjabloon/" + type);
-    },
+  getSjabloonOpId(type, id) {
+    return apiClient().get(baseUrl + "sjabloon/" + type + "/" + id);
+  },
 
-    getSjabloonOpId(type, id) {
-        return apiClient().get(baseUrl + "sjabloon/" + type + "/" +id);
-    },
+  verwijderSjabloon(type, id) {
+    return apiClient().delete(baseUrl + "sjabloon/" + type + "/" + id);
+  },
 
-    verwijderSjabloon(type, id) {
-        return apiClient().delete(baseUrl + "sjabloon/" + type + "/" +id)
-    },
+  updateSjabloon(type, id, sjabloon) {
+    return apiClient().patch(baseUrl + "sjabloon/" + type + "/" + id, sjabloon);
+  },
 
-    updateSjabloon(type, id, sjabloon) {
-        return apiClient().patch(baseUrl + "sjabloon/" + type + "/" +id, sjabloon)
-    },
+  saveSjabloon(type, sjabloon) {
+    return apiClient().post(baseUrl + "sjabloon/" + type, sjabloon);
+  },
 
-    saveSjabloon(type, sjabloon) {
-        return apiClient().post(baseUrl + "sjabloon/" + type, sjabloon)
-    },
+  printEtiketten(offset, data) {
+    return apiClientPdf().post(
+      baseUrl + "ledenlijst/etiket?offset=" + offset,
+      data
+    );
+  },
 
-    printEtiketten(offset, data) {
-        return apiClientPdf().post(baseUrl + "ledenlijst/etiket?offset=" + offset, data)
-    },
+  verstuurMail(bevestiging, formData) {
+    return apiClientMail().post(
+      baseUrl + "ledenlijst/mail?bevestig=" + bevestiging,
+      formData
+    );
+  },
 
-    verstuurMail(bevestiging, formData) {
-        return apiClientMail().post(baseUrl + "ledenlijst/mail?bevestig=" + bevestiging, formData)
-    },
+  saveEtiketSjabloon(sjabloon) {
+    return apiClient().post(baseUrl + "sjabloon/etiket/dummyid", sjabloon);
+  },
 
-    saveEtiketSjabloon(sjabloon) {
-        return apiClient().post(baseUrl + "sjabloon/etiket/dummyid", sjabloon)
-    },
+  getKolomType() {
+    return apiClient().get(baseUrl + "ledenlijst/kolom-type");
+  },
 
-    getKolomType() {
-        return apiClient().get(baseUrl + "ledenlijst/kolom-type");
-    },
+  postFuncties(data) {
+    return apiClient().post(baseUrl + "functie", data);
+  },
 
-    postFuncties(data) {
-        return apiClient().post(baseUrl + "functie", data);
-    },
+  getFunctieOpId(id) {
+    return apiClient().get(baseUrl + "functie/" + id);
+  },
 
-    getFunctieOpId(id) {
-        return apiClient().get(baseUrl + "functie/" + id,);
-    },
+  verwijderFunctie(id) {
+    return apiClient().delete(baseUrl + "functie/" + id);
+  },
 
-    verwijderFunctie(id) {
-        return apiClient().delete(baseUrl + "functie/" + id,);
-    },
+  pasFunctieAan(id) {
+    return apiClient().patch(baseUrl + "functie/" + id);
+  },
 
-    pasFunctieAan(id) {
-        return apiClient().patch(baseUrl + "functie/" + id,);
-    },
+  getIndividueleSteekkaart(id) {
+    return apiClient().get(baseUrl + "lid/" + id + "/steekkaart");
+  },
 
-    getIndividueleSteekkaart(id) {
-        return apiClient().get(baseUrl + "lid/" + id + "/steekkaart")
-    },
-
-    saveIndividueleSteekkaart(id, gegevens) {
-        return apiClient().patch(baseUrl + "lid/" + id + "/steekkaart", gegevens)
-    }
-
+  saveIndividueleSteekkaart(id, gegevens) {
+    return apiClient().patch(baseUrl + "lid/" + id + "/steekkaart", gegevens);
+  },
 };
