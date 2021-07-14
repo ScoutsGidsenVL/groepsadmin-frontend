fcb3018aed2b7e6638b386bae99db0d82838537f
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-07-14 09:46:26 +0200

    dev: aanpassen van vue router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "@/views/Dashboard";
 import Ledenlijst from "@/views/Ledenlijst";
 import Ledenaantallen from "@/views/Ledenaantallen";
@@ -58,7 +58,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
