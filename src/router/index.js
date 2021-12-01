64f7038fcf0db48e6e6b7aba976b1e578ca25823
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-12-01 09:18:51 +0100

    Router aanpassingen

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Dashboard from "@/views/Dashboard";
 import Ledenlijst from "@/views/Ledenlijst";
 import Ledenaantallen from "@/views/Ledenaantallen";
@@ -92,7 +92,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHashHistory("/"),
   routes,
 
 });
