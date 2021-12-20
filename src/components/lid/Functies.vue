3d2e18bcb35d9e37381eb23df841284d21352dd1
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-12-20 16:22:13 +0100

    Functies toevoegen in lid

--- src/components/lid/Functies.vue
+++ src/components/lid/Functies.vue
@@ -3,28 +3,31 @@
     <card>
       <template #title>
         <div class="d-flex col-12 justify-content-between">
-          <span> {{ title }}</span>
+          <span> Functies </span>
+        </div>
+        <div class="d-flex col-12">
+          <InputSwitch v-model="historiek"/>
+          <span class="ml-2 text-sm">Historiek weergeven</span>
+        </div>
+        <div :class="laden ? 'functies-loader' : ''">
+          <Indicator :is-loading="laden" :full-page="false" :height=55 :width=55></Indicator>
         </div>
       </template>
       <template #content>
-        <accordion :multiple="true">
-          <accordionTab v-for="(functies, index) in functies" :key="index">
-            <template #header>
-              <div class="d-flex col-12 justify-content-between">
-                {{ groepsNaam(index) }}
-              </div>
-            </template>
+        <accordion :multiple="true" v-if="!laden">
+          <accordionTab v-for="(functies, nummer) in actieveGroepen" :key="nummer" :header="groepsNaam(nummer)">
             <div v-for="(functie, index) in functies" :key="index">
-              <div v-if="functie.actief">
+              <div v-if="functie.actief" class="functie-wrapper">
                 <div class="flex-row">
                   <div class="justify-content-between">
                     <label>van {{ formatteerDatum(functie.begin) }}</label>
                     <Button
-                      icon="pi pi-times"
+                      v-if="lidMagFunctieStoppen('functies.' + nummer)"
+                      icon="pi pi-trash"
                       class="p-button-rounded p-button-outlined p-button-danger remove-button mr-1 mt-2"
                       @click="
                         $event.stopPropagation();
-                        end(functie.id);
+                        stopFunctie(functie, nummer);
                       "
                       title="Stop functie"
                     />
@@ -32,15 +35,55 @@
                 </div>
                 <div class="flex-row">
                   <div class="justify-content-start">
-                    <label class="ml-4 font-weight-bolder">{{
-                      functie.naam
-                    }}</label>
+                    <label class="ml-4 font-bold">{{ functie.naam }}</label>
                   </div>
                 </div>
+                <div class="border mt-3"></div>
+              </div>
+            </div>
+            <div v-for="(functie, index) in functies" :key="index">
+              <div v-show="!functie.actief && historiek" class="functie-wrapper"
+                   :class="!functie.actief ? 'inactive-function' : ''">
+                <div class="flex-row">
+                  <div class="justify-content-between">
+                    <label>van {{ formatteerDatum(functie.begin) }}&nbsp;</label>
+                    <label> tot {{ formatteerDatum(functie.einde) }}</label>
+                  </div>
+                </div>
+                <div class="flex-row">
+                  <div class="justify-content-start">
+                    <label class="ml-4 font-bold">{{
+                        functie.naam
+                      }}</label>
+                  </div>
+                </div>
+                <div class="border mt-3"></div>
               </div>
             </div>
           </accordionTab>
         </accordion>
+        <div v-if="historiek && !laden">
+          <accordion :multiple="true">
+            <accordionTab v-for="(functies, index) in nietActieveGroepen" :key="index" :header="inactieveGroepsNaam(index)">
+              <div v-for="(functie, index) in functies" :key="index" class="inactive-function">
+                <div class="flex-row">
+                  <div class="justify-content-between">
+                    <label>van {{ formatteerDatum(functie.begin) }}&nbsp;</label>
+                    <label> tot {{ formatteerDatum(functie.einde) }}</label>
+                  </div>
+                </div>
+                <div class="flex-row">
+                  <div class="justify-content-start">
+                    <label class="ml-4 font-bold">{{
+                        functie.naam
+                      }}</label>
+                  </div>
+                </div>
+                <div class="border mt-3"></div>
+              </div>
+            </accordionTab>
+          </accordion>
+        </div>
       </template>
     </card>
   </div>
@@ -48,37 +91,93 @@
 
 <script>
 import DateUtil from "@/services/dates/DateUtil";
+import Indicator from "@/components/global/Indicator";
+import rechtenService from "@/services/rechten/rechtenService";
 
 export default {
   name: "Functies",
+  components: {Indicator},
+  data() {
+    return {
+      historiek: false,
+      confirmDialog: false,
+      teStoppenFunctie: {}
+    }
+  },
   props: {
     modelValue: {
-      type: Object,
+      type: Object, Array
     },
-    title: {
-      type: String,
-    },
+    lid: {
+      type: Object
+    }
   },
   computed: {
-    functies() {
-      return this.modelValue;
+    actieveGroepen() {
+      return Object.fromEntries(Object.entries(this.modelValue).filter(([key]) => this.modelValue[key].active));
+    },
+    nietActieveGroepen() {
+      return Object.fromEntries(Object.entries(this.modelValue).filter(([key]) => !this.modelValue[key].active));
+    },
+    laden() {
+      return this.$store.getters.groepenLaden || this.$store.getters.functiesLaden;
     },
   },
   methods: {
     groepsNaam(groepsnummer) {
-      const groep = this.$store.getters.groepByNummer(groepsnummer);
-      return groep.naam + " - " + groepsnummer;
+      let groep = this.$store.getters.groepByNummer(groepsnummer);
+      if (groep) {
+        return groep.naam + " - " + groepsnummer;
+      }
     },
+
+    boodschapStoppenFunctie() {
+      let message = "";
+      if (this.teStoppenFunctie) {
+        message = "Ben je zeker dat je de functie " + this.teStoppenFunctie.naam + " wil stoppen?"
+      }
+      return message;
+    },
+
+    inactieveGroepsNaam(groepsnummer) {
+      let groep = this.$store.getters.inactieveGroepByNummer(groepsnummer);
+      if (groep) {
+        return groep.naam + " - " + groepsnummer;
+      }
+    },
+
+    lidMagFunctieStoppen(sectie) {
+      return rechtenService.hasPermission(this.lid, sectie);
+    },
+
     getFunctie(functieId) {
       return this.$store.getters.functieById(functieId);
     },
     formatteerDatum(datum) {
       return DateUtil.formatteerDatum(datum);
     },
-    end(id) {
-      console.log(id);
+    stopFunctie(functie, groepsnummer) {
+      this.teStoppenFunctie = Object.assign({}, functie);;
+      this.$confirm.require({
+        message: this.boodschapStoppenFunctie(),
+        header: "Functie stoppen",
+        icon: "pi pi-exclamation-triangle",
+        acceptIcon: "pi pi-check",
+        rejectIcon: "pi pi-times",
+        acceptClass: "approve-button",
+        rejectClass: "reject-button",
+        accept: () => {
+          this.teStoppenFunctie.einde = new Date();
+          this.$emit('updateLid',  {functie: this.teStoppenFunctie, groepsnummer: groepsnummer });
+        },
+        reject: () => {
+          this.$confirm.close();
+        }
+
+      })
     },
   },
+
 };
 </script>
 
--- src/views/Lid.vue
+++ src/views/Lid.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container-fluid md:w-90 lg:ml-4">
+  <Loader
+    :showLoader="laden"
+  ></Loader>
+  <div class="container-fluid md:w-90 lg:ml-4 md:pl-8">
     <div class="hidden lg:block lg:ml-8">
       <Breadcrumb :home="home" :model="breadcrumbItems" class="ml-4 mt-4"/>
     </div>
@@ -9,20 +12,28 @@
         <div class="row lg:ml-8">
           <div class="col-12 col-lg-6 col-xl-4">
             <persoonlijk v-model="lid"></persoonlijk>
+            <groepseigen-gegevens
+              v-if="groepseigenVelden && Object.keys(groepseigenVelden).length > 0"
+              v-model="groepseigenVelden"
+              :title="'Groepseigen gegevens'"
+            ></groepseigen-gegevens>
           </div>
           <div class="col-12 col-lg-6 col-xl-4">
             <adressen v-model="lid" :title="'Adressen'"></adressen>
             <contacten v-model="lid" :title="'Contacten'"></contacten>
-            <groepseigen-gegevens
-              v-model="lid.groepseigenVelden"
-              :title="'Groepseigen gegevens'"
-            ></groepseigen-gegevens>
+
           </div>
           <div class="col-12 col-lg-12 col-xl-4">
             <functies
-              :title="'Functies'"
               v-model="gesorteerdeFuncties"
+              @updateLid="updateFuncties"
+              :lid="lid"
             ></functies>
+            <functies-toevoegen
+              v-model="gesorteerdeFuncties"
+              :lid="lid"
+              v-if="magFunctiesToevoegen"
+            ></functies-toevoegen>
           </div>
         </div>
         <Button
@@ -44,8 +55,11 @@ import Contacten from "@/components/lid/Contacten";
 import GroepseigenGegevens from "@/components/lid/GroepseigenGegevens";
 import Functies from "@/components/lid/Functies";
 import RestService from "@/services/api/RestService";
-import specialeFuncties from "@/services/functies/specialeFuncties";
+import specialeFuncties from "@/services/functies/SpecialeFuncties";
 import Footer from "@/components/global/Footer";
+import Loader from "@/components/global/Loader";
+import rechtenService from "@/services/rechten/rechtenService";
+import FunctiesToevoegen from "@/components/lid/FunctiesToevoegen";
 
 export default {
   name: "Lid",
@@ -57,6 +71,8 @@ export default {
     Persoonlijk,
     LidBovenBalk,
     Adressen,
+    Loader,
+    FunctiesToevoegen
   },
   data() {
     return {
@@ -75,9 +91,11 @@ export default {
       aangepasteContacten: false,
       aangepasteFuncties: false,
       aangepasteGroepseigenVelden: false,
+      eigenProfiel: false,
       id: "",
 
       changed: true,
+      loadingLid: true,
       lid: {
         voornaam: "",
         achternaam: "",
@@ -89,6 +107,7 @@ export default {
         verbondsgegevens: {},
       },
       gesorteerdeFuncties: {},
+      groepseigenVelden: {}
     };
   },
 
@@ -115,6 +134,7 @@ export default {
     },
     "lid.functies": {
       handler: function () {
+        console.log('functies aangepast');
       },
       deep: true,
     },
@@ -151,43 +171,103 @@ export default {
       console.log(this.lid);
     },
 
+    updateFuncties({functie, groepsnummer}) {
+      let groep = this.$store.getters.groepByNummer(groepsnummer);
+      let lid = {
+        functies: [
+          {
+            functie: functie.id,
+            groep: groep.id,
+            einde: new Date(),
+            begin: functie.begin
+          }
+        ]
+      };
+      RestService.updateLid(this.lid.id, lid).then(res => {
+        if (res.status === 200) {
+          this.$toast.add({
+            severity: "success",
+            summary: "Wijzigingen",
+            detail: "Functie is gestopt",
+          });
+          this.lid = res.data;
+          this.sorteerFuncties();
+        }
+      });
+    },
+
     getProfiel() {
       this.lid = this.$store.getters.profiel;
       this.sorteerFuncties();
+      this.filterGroepsEigenVelden();
     },
 
     getLid(id) {
+      this.loadingLid = true
       RestService.getLid(id).then((res) => {
         this.lid = res.data;
         if (id === "profiel") {
+          this.eigenProfiel = true;
           this.$store.commit("setProfiel", res.data);
         }
         this.sorteerFuncties();
       });
     },
 
+    filterGroepsEigenVelden() {
+      this.groepseigenVelden =  Object.fromEntries(Object.entries( this.lid.groepseigenVelden).filter(([key]) => this.lid.groepseigenVelden[key].schema.length > 0));
+    },
+
     sorteerFuncties() {
-      this.gesorteerdeFuncties = {};
-      this.lid.functies.forEach((functie) => {
-        if (!(functie.groep in this.gesorteerdeFuncties)) {
-          this.gesorteerdeFuncties[functie.groep] = [];
+      this.$store.commit("setGroepenLaden", true);
+      let ongesorteerdeFuncties = {};
+      let functies = [];
+      if (this.eigenProfiel) {
+         functies = this.$store.getters.profiel.functies;
+      } else {
+        functies = this.lid.functies;
+      }
+      functies.forEach((functie) => {
+        if (!(functie.groep in ongesorteerdeFuncties)) {
+          ongesorteerdeFuncties[functie.groep] = [];
         }
 
         const functieById = this.$store.getters.functieById(functie.functie);
-
-        let functieObject = {
-          id: functieById.id,
-          naam: functieById.beschrijving,
-          begin: functie.begin,
-          einde: functie.einde,
-          specialeFunctie:
-            functie.functie === specialeFuncties.FV ||
-            functie.functie === specialeFuncties.VGA,
-          actief: !functie.einde,
-        };
-        this.gesorteerdeFuncties[functie.groep].push(functieObject);
+        if (functieById) {
+          let functieObject = {
+            id: functieById.id,
+            naam: functieById.beschrijving,
+            begin: functie.begin,
+            einde: functie.einde,
+            specialeFunctie:
+              functie.functie === specialeFuncties.FV ||
+              functie.functie === specialeFuncties.VGA,
+            actief: !functie.einde,
+          };
+          ongesorteerdeFuncties[functie.groep].push(functieObject);
+          if (!ongesorteerdeFuncties[functie.groep].active) {
+            ongesorteerdeFuncties[functie.groep].active = functieObject.actief;
+          }
+        }
       });
+
+      let inactieveGroepen = Object.entries(ongesorteerdeFuncties).filter(([key]) => !ongesorteerdeFuncties[key].active);
+      inactieveGroepen.forEach(groep => {
+        this.$store.dispatch('addGroep', groep[0]);
+      })
+
+      this.gesorteerdeFuncties = Object.keys(ongesorteerdeFuncties).sort().reduce(
+        (obj, key) => {
+          obj[key] = ongesorteerdeFuncties[key];
+          return obj;
+        },
+        {}
+      )
+      this.$store.commit("setGroepenLaden", false);
+      this.loadingLid = false;
     },
+
+
   },
 
   computed: {
@@ -196,6 +276,12 @@ export default {
         this.lid.vgagegevens.voornaam + " " + this.lid.vgagegevens.achternaam
       );
     },
+    laden() {
+      return this.loadingLid;
+    },
+    magFunctiesToevoegen() {
+      return rechtenService.canBeShowed(this.lid, 'functies.')
+    }
   },
 };
 </script>
