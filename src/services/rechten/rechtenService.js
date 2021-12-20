e758cf288a2afcbfd64eb68604de0724f00fd96d
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-12-20 15:24:56 +0100

    Rechtenservice uitbouwen

--- src/services/rechten/rechtenService.js
+++ src/services/rechten/rechtenService.js
@@ -9,5 +9,31 @@ export default {
       }
     })
     return result;
+  },
+
+  hasPermission: function (lid, sectie) {
+    let patchObject = lid.links.find(obj => {
+      return obj.method === "PATCH"
+    });
+    if (patchObject) {
+      return patchObject.secties.includes(sectie);
+    }
+    return false;
+  },
+
+
+
+  canBeShowed: function (lid, type) {
+    let patchObject = lid.links.find(obj => {
+      return obj.method === "PATCH"
+    });
+    if (patchObject) {
+      return patchObject.secties.find(element => {
+        if (element.includes(type)) {
+          return true;
+        }
+      });
+    }
+    return false;
   }
 };
