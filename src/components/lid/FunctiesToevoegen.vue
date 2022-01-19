daaa5c872d91b5977f5b9276f6280723579564ff
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-01-19 12:01:34 +0100

    Component toegevoegd aan vcs

---
+++ src/components/lid/FunctiesToevoegen.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="functies-card mb-4">
+    <card>
+      <template #title>
+        <div class="d-flex col-12 justify-content-between">
+          <span> Functies toevoegen</span>
+        </div>
+        <div :class="laden ? 'functies-loader' : ''">
+          <Indicator :is-loading="laden" :full-page="false" :height=55 :width=55></Indicator>
+        </div>
+      </template>
+      <template #content>
+        <accordion :multiple="true">
+          <accordionTab v-for="(groep, index) in groepEnfuncties" :key="index"
+                        :header="groep.naam + ' - ' + groep.groepsnummer">
+            <div class="row">
+              <div v-for="(functie, index) in gesorteerdeFuncties(groep.functies, 'verbond')" :key="index"
+                   class="col-md-6">
+                <checkbox :label="functie.beschrijving" :model-value="isSelected(functie, groep.groepsnummer)"
+                          @changeValue="voegToeOfVerwijderFunctie(functie, groep.groepsnummer)"></checkbox>
+              </div>
+            </div>
+            <div v-if="gesorteerdeFuncties(groep.functies, 'groep').length > 0">
+            <div class="border mt-3 mb-2"></div>
+              <label class="mb-2"><strong>Groepseigen functies</strong></label>
+              <div class="row">
+                <div v-for="(functie, index) in gesorteerdeFuncties(groep.functies, 'groep')" :key="index"
+                     class="col-md-6">
+                  <checkbox :label="functie.beschrijving" :model-value="isSelected(functie, groep.groepsnummer)"
+                            @changeValue="voegToeOfVerwijderFunctie(functie, groep.groepsnummer)"></checkbox>
+                </div>
+              </div>
+            </div>
+          </accordionTab>
+        </accordion>
+      </template>
+    </card>
+  </div>
+</template>
+
+<script>
+import rechtenService from "@/services/rechten/rechtenService";
+import BaseCheckboxLeftFunctieSelect from "@/components/input/BaseCheckboxLeftFunctieSelect";
+
+export default {
+  name: "FunctiesToevoegen",
+  components: {
+    'checkbox': BaseCheckboxLeftFunctieSelect
+  },
+  data() {
+    return {
+      laden: false,
+      functiesEnGroepenGeladen: false,
+      showFunctieToevoegen: false,
+      groepEnfuncties: [],
+    }
+  },
+  props: {
+    modelValue: {
+      type: Object, Array
+    },
+    lid: {
+      type: Object
+    }
+  },
+  computed: {
+    huidigLid() {
+      return this.lid;
+    }
+  },
+  mounted() {
+    this.functiesEnGroepen();
+  },
+  methods: {
+    gesorteerdeFuncties(functies, type) {
+      functies.sort(function (a, b) {
+        if (a.beschrijving < b.beschrijving) {
+          return -1;
+        }
+        if (a.beschrijving > b.beschrijving) {
+          return 1;
+        }
+        return 0;
+      })
+      let gefilterdeFuncties = functies.filter(obj => {
+        return obj.type === type;
+      })
+      return gefilterdeFuncties;
+    },
+    functiesEnGroepen() {
+      this.groepEnfuncties = [];
+      this.$store.getters.groepen.forEach(groep => {
+        if (rechtenService.hasPermission(this.huidigLid, 'functies.' + groep.groepsnummer)) {
+          let tempGroep = groep;
+          tempGroep.functies = [];
+          this.$store.getters.functies.forEach(functie => {
+            let bestaandeFunctie = false;
+            this.huidigLid.functies.forEach(lidFunctie => {
+              if (lidFunctie.groep === groep.groepsnummer && lidFunctie.functie === functie.id) {
+                bestaandeFunctie = true;
+              }
+            })
+            if (functie.groepen.indexOf(tempGroep.groepsnummer) != -1 && !bestaandeFunctie) {
+              tempGroep.functies.push(functie);
+            }
+          });
+          this.groepEnfuncties.push(tempGroep);
+        }
+      });
+
+      this.functiesEnGroepenGeladen = true;
+      this.showFunctieToevoegen = false;
+
+      this.groepEnfuncties.forEach(groep => {
+        this.showFunctieToevoegen |= rechtenService.hasPermission(this.huidigLid, 'functies.' + groep.groepsnummer);
+      });
+
+    },
+    groepsNaam(index) {
+      let groep = this.$store.getters.groepen[index];
+      if (groep) {
+        return groep.naam + " - " + groep.groepsnummer;
+      }
+    },
+    voegToeOfVerwijderFunctie(functie, groepsnummer) {
+      let functieInstantie = {};
+      functieInstantie.functie = functie.id;
+      functieInstantie.groep = groepsnummer;
+      functieInstantie.begin = '2016-01-01T00:00:00.000+01:00'; // set static date
+      functieInstantie.temp = "tijdelijk";
+
+      let bestaandeFunctie = false;
+
+      for (let [index, val] of this.huidigLid.functies.entries()) {
+        if (val.functie === functie.id && val.groep === groepsnummer) {
+          bestaandeFunctie = true;
+          this.huidigLid.functies.splice(index, 1);
+        }
+      }
+      if (!bestaandeFunctie) {
+        this.huidigLid.functies.push(functieInstantie);
+      }
+    },
+    isSelected(functie, groepsnummer) {
+      let geselecteerd = false;
+      this.huidigLid.functies.forEach(lidFunctie => {
+        if (functie.id === lidFunctie.functie && lidFunctie.groep === groepsnummer) {
+          geselecteerd = true;
+        }
+      })
+      return geselecteerd;
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
