9a5366809e65be7cd5f245cc15ce2a29c80fbf24
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-01-19 11:24:41 +0100

    knop toegevoegd

---
+++ src/components/buttons/Steekkaart.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button icon="fa fa-notes-medical" class="p-button-rounded custom-knop algemene-lid-knop" title="Individuele steekkaart"/>
+</template>
+
+<script>
+export default {
+  name: "Steekkaart"
+}
+</script>
+
+<style scoped>
+
+</style>
