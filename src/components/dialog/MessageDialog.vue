a108ab99b11db8d206e14a32cca7460207d930b1
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-03-21 12:18:14 +0100

    Boodschapdialog toegevoegd

---
+++ src/components/dialog/MessageDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <Dialog
+    :header="header"
+    v-model:visible="openDialog"
+    :style="{ width: '550px' }"
+    :modal="true"
+    :closable="false"
+  >
+    <div class="confirmation-content">
+      <span v-html="message"></span>
+    </div>
+    <template #footer>
+      <Button
+        label="Begrepen"
+        icon="pi pi-check"
+        @click="sluiten"
+        class="p-button-text approve-button"
+
+      />
+    </template>
+  </Dialog>
+</template>
+
+<script>
+export default {
+  name: "MessageDialog",
+  props: {
+    message: {
+      type: String,
+    },
+    header: {
+      type: String,
+      default: "Opgelet"
+    },
+    dialogVisible: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String
+    }
+  },
+  methods: {
+    sluiten() {
+      this.$emit("close");
+    },
+  },
+  computed: {
+    openDialog() {
+      return this.dialogVisible;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
