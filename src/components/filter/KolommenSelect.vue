413d3d2d9fbda01de3721b548806faee82c6c6c3
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-04-12 13:09:59 +0200

    eerste opzet kolommenselect

--- src/components/filter/KolommenSelect.vue
+++ src/components/filter/KolommenSelect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="col-lg-2 ">
-    <div @click="toggle" class="clickable btn-primary col-12 kolom-select text-align-left">Kolommen</div>
-    <div v-show="toggleMenu" class="position-absolute z999 bg-white col-12 mt-2">
+    <div @click="toggleMenu = !toggleMenu" class="clickable btn-primary col-12 kolom-select text-align-left">Kolommen</div>
+    <div v-show="toggleMenu" class="position-absolute z999 bg-white col-2 mt-2 filter-border">
       <div v-for="(item, key) in actieveKolommen" :key="key">
-        <base-checkbox :label="item.header" class="text-align-left"></base-checkbox>
+        <base-checkbox :label="item.header" class="text-align-left" @check="voegKolomToe"></base-checkbox>
       </div>
     </div>
   </div>
@@ -27,41 +27,14 @@ export default {
     return {
       toggleMenu: false,
       geselecteerdeKolommen: null,
-      groupedCities: [{
-        label: 'Germany', code: 'DE',
-        items: [
-          {label: 'Berlin', value: 'Berlin'},
-          {label: 'Frankfurt', value: 'Frankfurt'},
-          {label: 'Hamburg', value: 'Hamburg'},
-          {label: 'Munich', value: 'Munich'}
-        ]
-      },
-        {
-          label: 'USA', code: 'US',
-          items: [
-            {label: 'Chicago', value: 'Chicago'},
-            {label: 'Los Angeles', value: 'Los Angeles'},
-            {label: 'New York', value: 'New York'},
-            {label: 'San Francisco', value: 'San Francisco'}
-          ]
-        },
-        {
-          label: 'Japan', code: 'JP',
-          items: [
-            {label: 'Kyoto', value: 'Kyoto'},
-            {label: 'Osaka', value: 'Osaka'},
-            {label: 'Tokyo', value: 'Tokyo'},
-            {label: 'Yokohama', value: 'Yokohama'}
-          ]
-        }]
     }
   },
   mounted() {
 
   },
   methods: {
-    toggle() {
-      this.toggleMenu = !this.toggleMenu;
+    voegKolomToe() {
+
     }
   }
 }
