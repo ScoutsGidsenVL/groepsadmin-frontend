2574448457b1492f7d2d69d6486ae7d0e076388a
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-06-13 10:18:55 +0200

    indien het gaat om de publieke pagina moet geen token worden meegegeven

--- src/services/api/ApiClient.js
+++ src/services/api/ApiClient.js
@@ -2,13 +2,20 @@ import axios from "axios";
 import store from "@/store";
 
 export default function apiClient() {
+
+  let headers = {
+    Accept: "application/json",
+    responseType: "arraybuffer",
+    "Content-Type": "application/json",
+  };
+
+  // Publieke pagina mag geen restService aanroepen met token
+  if(!window.location.pathname.startsWith("/groepsadmin/frontend/formulier/")) {
+    headers.Authorization = "bearer " + store.getters.token;
+  }
+
   return axios.create({
     withCredentials: false,
-    headers: {
-      Accept: "application/json",
-      Authorization: "bearer " + store.getters.token,
-      responseType: "arraybuffer",
-      "Content-Type": "application/json",
-    },
+    headers: headers
   });
 }
