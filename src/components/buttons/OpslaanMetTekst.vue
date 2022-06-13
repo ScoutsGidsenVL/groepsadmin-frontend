b4fc7c4514f5caa218d609107fe4b8ed306cc7ea
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-06-13 10:30:11 +0200

    knop toegevoegd

---
+++ src/components/buttons/OpslaanMetTekst.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button icon="pi pi-save" class="p-button opslaan-knop " @click="opslaan" label="Opslaan"></Button>
+</template>
+<script>
+export default {
+  name: "Opslaan",
+  methods: {
+    opslaan() {
+      this.$emit('opslaan');
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
