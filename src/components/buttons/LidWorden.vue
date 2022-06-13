c5132a4bce04706c836ae013b3d6537d7cb240d5
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-06-13 10:29:33 +0200

    Knop toegevoegd

---
+++ src/components/buttons/LidWorden.vue
@@ -0,0 +1,23 @@
+<template>
+  <Button class="algemene-lid-knop" title="Lid worden" @click="opslaan" :disabled="disabled" >Lid worden</Button>
+</template>
+
+<script>
+export default {
+  name: "NieuwLid",
+  props: {
+    disabled: {
+      type: Boolean
+    }
+  },
+  methods: {
+    opslaan() {
+      this.$emit('opslaan');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
