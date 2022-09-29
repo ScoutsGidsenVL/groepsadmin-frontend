a37cd0c2ee6f6aacc6af0c1c44012060893362b7
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-09-29 13:44:41 +0200

    interceptors toegevoegd

--- src/services/api/ApiClient.js
+++ src/services/api/ApiClient.js
@@ -1,21 +1,40 @@
 import axios from "axios";
 import store from "@/store";
 
+
 export default function apiClient() {
 
-  let headers = {
-    Accept: "application/json",
-    responseType: "arraybuffer",
-    "Content-Type": "application/json",
-  };
+    let headers = {
+        Accept: "application/json",
+        responseType: "arraybuffer",
+        "Content-Type": "application/json",
+    };
 
-  // Publieke pagina mag geen restService aanroepen met token
-  if(!window.location.pathname.startsWith("/groepsadmin/frontend/formulier/")) {
-    headers.Authorization = "bearer " + store.getters.token;
-  }
+    // Publieke pagina mag geen restService aanroepen met token
+    if (!window.location.pathname.startsWith("/groepsadmin/frontend/formulier/")) {
+        headers.Authorization = "bearer " + store.getters.token;
+    }
 
-  return axios.create({
-    withCredentials: false,
-    headers: headers
-  });
+    const instance = axios.create({
+        withCredentials: false,
+        headers: headers,
+    });
+
+    instance.interceptors.response.use((response) => {
+        return response
+    }, (error) => {
+        console.log(error.response.data.status)
+        if (error.response.data.status !== 500) {
+            return error
+        } else {
+            console.log('opgevangen error')
+            console.log(error.response.data.status !== 500);
+        }
+    })
+    // return axios.create({
+    //   withCredentials: false,
+    //   headers: headers,
+    // });
+
+    return instance;
 }
