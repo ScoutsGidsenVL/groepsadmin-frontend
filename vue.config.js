fae081048d9ea26e6ecd2a442e736bf5c88ad45e
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2022-11-15 10:05:57 +0100

    keycloakaanpassingen terugzetten

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ process.env.VUE_APP_VERSION = require("./package.json").version;
 module.exports = {
   devServer: {
     port: 3000,
-    proxy: "http://localhost:9090",
+    proxy: "http://localhost:8080",
   },
   publicPath: process.env.NODE_ENV === 'production'
       ? '/groepsadmin/frontend/'
