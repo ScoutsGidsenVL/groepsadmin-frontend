5702d4a3eae24d434e7bf489e55050e028dcaadc
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2023-01-17 15:15:06 +0100

    bug met kolommen opgelost

--- src/services/leden/Ledenlijst.js
+++ src/services/leden/Ledenlijst.js
@@ -52,6 +52,7 @@ export default {
             huidigeFilter: {},
             leden: [],
             actieveKolommen: [],
+            nonActieveKolommen: [],
             filters: [],
             geselecteerdeLeden: [],
             deelFilter: false,
