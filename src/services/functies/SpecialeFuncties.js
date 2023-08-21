9f96028554f902201af20a88c27d49929dc170e3
jressen <Johnny@cineworks.be>
2023-08-21 11:41:20 +0200

    Speciale functies in functie van AVGA

--- src/services/functies/SpecialeFuncties.js
+++ src/services/functies/SpecialeFuncties.js
@@ -1,6 +1,7 @@
 const specialeFuncties = {
   VGA: "d5f75b320b812440010b812555970393",
   FV: "d5f75b320b812440010b812553d5032e",
+  AVGA: "8a95af9385ad9b880185c035ee740010"
 };
 
 export default specialeFuncties;
