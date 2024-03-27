82c765c2ee139b40c08448c1680031b07b05c2dd
jressen <Johnny@cineworks.be>
2024-03-27 11:14:58 +0100

    T6554: opzet aanwezigheden

--- src/views/Aanwezigheden.vue
+++ src/views/Aanwezigheden.vue
@@ -11,38 +11,58 @@
           <Breadcrumb :home="home" :model="breadcrumbItems" class="ml-4 mt-4 md:ml-6 mb-8"/>
         </div>
         <Loader
-          :showLoader="isLoadingActiviteiten"
+          :showLoader="isLoadingAanwezigheden"
         ></Loader>
         <div class="lg:ml-8">
           <div class="lg:ml-6">
-            <div class="flex justify-content-end">
-            <Button type="button" label="Activiteit toevoegen" @click="voegActivteitToe()" icon="far fa-plus"
-                    class="actie-button mr-1"/>
-            </div>
+            <label class="d-flex justify-content-start">Aanwezigheden registreren</label>
+
             <data-table
-              ref="activiteitenlijst"
-              :value="activiteiten"
+              ref="ledenlijst"
+              :value="sorteerLeden"
+              v-model:selection="aanwezigeLeden"
+              dataKey="id"
+              editMode="cell"
+              @cell-edit-complete="bewerkCell"
+              rowGroupMode="subheader"
+              groupRowsBy="functie.omschrijving"
+              sortMode="single"
+              sortField="functie.omschrijving"
+              :sortOrder="1"
               stripedRows
               showGridlines
-              responsiveLayout="scroll"
-              @row-click="registreerAanwezigheden"
-              class="p-datatable-sm mt-4 activiteitentabel"
+              responsiveLayout="stack"
+              class="p-datatable-sm mt-4 aanwezighedenTabel"
             >
-              <column field="werkjaar" header="Werkjaar"></column>
-              <column field="periode" header="Periode"></column>
-              <column field="omschrijving" header="Omschrijving"></column>
-              <column field="prijs" header="prijs"></column>
+              <column field="functie.omschrijving" header="Functie"></column>
+              <column selectionMode="multiple" headerStyle="width: 3rem"></column>
+              <column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header" style="width: 25%">
+                <template #body="{ data, field }">
+                  {{ field === 'prijs' ? formateerBedrag(data[field]) : data[field] }}
+                </template>
+                <template #editor="{ data, field }">
+                  <template v-if="field === 'aantal'">
+                    <input-text v-model="data[field]" autofocus />
+                  </template>
+                  <template v-if="field === 'prijs'">
+                    <input-number v-model="data[field]" mode="currency" currency="EUR" locale="de-DE" autofocus />
+                  </template>
+                </template>
+              </column>
               <template #empty>
-                Geen activiteiten gevonden.
+                Geen leden gevonden.
               </template>
-
+              <template #groupheader="slotProps">
+                <div class="flex align-items-center gap-2">
+                  <span class="font-bold ml-5">{{ slotProps.data.functie.omschrijving }}</span>
+                </div>
+              </template>
             </data-table>
           </div>
         </div>
       </div>
   </div>
   <Footer/>
-
 </template>
 
 
@@ -52,30 +72,33 @@ import SideMenu from "@/components/global/Menu.vue";
 import Loader from "@/components/global/Loader.vue";
 import ConfirmDialog from "@/components/dialog/ConfirmDialog.vue";
 import {toRefs} from "@vue/reactivity";
-import ActiviteitenService from "@/services/activiteiten/ActiviteitenService";
 import Footer from "@/components/global/Footer.vue";
+import AanwezighedenService from "@/services/aanwezigheden/AanwezighedenService";
+import InputNumber from "primevue/inputnumber";
 
 export default {
-  name: "Aanvragen",
+  name: "Aanwezigheden",
   components: {
     Footer,
     Loader,
     ConfirmDialog,
     SideMenu,
-    IngelogdLid
+    IngelogdLid,
+    InputNumber
   },
   setup() {
-
     const {
       state,
-      registreerAanwezigheden,
-      voegActivteitToe
-    } = ActiviteitenService.activiteitenSpace();
+      bewerkCell,
+      formateerBedrag,
+      sorteerLeden
+    } = AanwezighedenService.aanwezighedenSpace();
 
     return {
-      ...toRefs(state),
-      registreerAanwezigheden,
-      voegActivteitToe
+      bewerkCell,
+      formateerBedrag,
+      sorteerLeden,
+      ...toRefs(state)
     }
   }
 }
