137ea29273633ca52a2691ed1ee0a3e4af0f1c17
jressen <Johnny@cineworks.be>
2024-03-27 11:15:29 +0100

    T6554: aanpassingen activiteiten

--- src/views/Activiteiten.vue
+++ src/views/Activiteiten.vue
@@ -24,9 +24,9 @@
 
       <div class="lg:ml-8">
         <div class="lg:ml-6">
-
+          <label class="d-flex justify-content-start">Betalende activiteiten</label>
           <div class="flex justify-content-between ">
-            <div class="col-12 col-lg-6 col-xl-4 groep-select justify-content-start" v-if="!groepenLaden">
+            <div class="col-12 col-lg-6 col-xl-4 groep-select justify-content-start ml--2 " v-if="!groepenLaden">
               <BaseDropdown
                 :options="groepenArray"
                 :model-value="selectedGroep"
@@ -42,39 +42,47 @@
             ref="activiteitenlijst"
             :value="activiteiten"
             stripedRows
+            :scrollable="true"
             showGridlines
-            responsiveLayout="scroll"
+            size="small"
             class="p-datatable-sm activiteitentabel"
           >
-            <column field="werkjaar" header="Werkjaar">
+            <column field="werkjaar" header="Werkjaar" style="width: 50px">
               <template #body="slotProps">
                 <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer">
                   {{ slotProps.data.werkjaar }}
                 </div>
               </template>
             </column>
-            <column field="periode" header="Periode">
+            <column field="periode" header="Periode" style="width: 250px">
               <template #body="slotProps">
                 <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer">
                   {{ formatteerPeriode(slotProps.data) }}
                 </div>
               </template>
             </column>
-            <column field="omschrijving" header="Omschrijving">
+            <column field="omschrijving" header="Omschrijving" style="width: 400px">
               <template #body="slotProps">
-                <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer">
+                <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer ellipsis">
                   {{ slotProps.data.omschrijving }}
                 </div>
               </template>
             </column>
-            <column field="prijs" header="Prijs">
+            <column field="functies" header="Functies" style="width: 300px">
               <template #body="slotProps">
-                <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer">
-                  {{ slotProps.data.prijs }}
+                <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer ellipsis">
+                  {{ formatteerFunctieOmschrijving(slotProps.data.functies) }}
                 </div>
               </template>
             </column>
-            <column field="acties" header="Acties" style="max-width: 50px">
+            <column field="prijs" header="Prijs" style="width: 80px">
+              <template #body="slotProps">
+                <div @click="registreerAanwezigheden(slotProps.data.id)" class="cursor-pointer">
+                  {{ slotProps.data.prijs }} €
+                </div>
+              </template>
+            </column>
+            <column field="acties" header="Acties" style="width: 150px">
               <template #body="slotProps">
                 <div class="flex justify-content-between">
                   <i class="fas fa-users-medical mr-3 cursor-pointer" style="font-size: 1.5rem"
@@ -84,13 +92,11 @@
                   <i class="fas fa-trash cursor-pointer" style="font-size: 1.5rem"
                      title="Activiteit verwijderen" @click="verwijderActiviteit(slotProps.data.id)"></i>
                 </div>
-
               </template>
             </column>
             <template #empty>
               Geen activiteiten gevonden.
             </template>
-
           </data-table>
         </div>
       </div>
@@ -133,7 +139,8 @@ export default {
       verwijderActiviteit,
       bevestigVerwijderen,
       annuleerVerwijderen,
-      formatteerPeriode
+      formatteerPeriode,
+      formatteerFunctieOmschrijving
     } = ActiviteitenService.activiteitenSpace();
 
     return {
@@ -144,7 +151,8 @@ export default {
       verwijderActiviteit,
       bevestigVerwijderen,
       annuleerVerwijderen,
-      formatteerPeriode
+      formatteerPeriode,
+      formatteerFunctieOmschrijving
     }
   }
 }
