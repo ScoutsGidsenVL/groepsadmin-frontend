d7c789c47098d4b2c0583632bfec96ae300219ec
Koen De Voegt <kdv@scoutsengisdenvlaanderen.be>
2024-12-16 17:20:16 +0100

    T6087 - Activiteiten op ledenlijst

--- src/views/Ledenlijst.vue
+++ src/views/Ledenlijst.vue
@@ -71,6 +71,8 @@
                   <div class="hidden md:block">
                     <Button type="button" label="Nieuw lid" @click="gaNaar('lidToevoegen')" icon="far fa-user-plus"
                             class="actie-button mr-1" v-if="magNieuwLidAanmaken()"/>
+                    <Button type="button" label="Activiteiten" @click="gaNaar('deelnemenActiviteit')" icon="far fa-campground"
+                            class="actie-button mr-1"/>
                     <Button type="button" label="Etiketten" @click="gaNaar('etiket')" icon="pi pi-tags"
                             class="actie-button mr-1"/>
                     <Button type="button" label="Mail" @click="gaNaar('email')" icon="pi pi-envelope"
