bd7af45683d0fefa251a15a1f36c9c02026d8274
Koen De Voegt <kdv@scoutsengisdenvlaanderen.be>
2025-01-14 15:38:54 +0100

    T6087 - fix werkjaar aanmaken/bewerken activiteit

--- src/services/activiteiten/ActiviteitenService.js
+++ src/services/activiteiten/ActiviteitenService.js
@@ -288,6 +288,7 @@ export default {
             }
 
             state.isLoadingActiviteiten = true;
+            delete state.activiteit.werkjaar;
             state.activiteit.van = DateUtil.formatteerDatumVoorApi(state.activiteit.van);
             state.activiteit.tot = DateUtil.formatteerDatumVoorApi(state.activiteit.tot);
             state.activiteit.groep = props.groep;
