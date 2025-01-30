7dba6560d5ecd3ba9c0ab363a479f42e7955b4a5
TinusVL <TinusVL@users.noreply.github.com>
2025-01-30 16:47:41 +0100

    Revert "Small fix"
    This reverts commit 9883835c2946437699fc3a3bdb8159252046c669.

--- src/components/filter/LedenlijstFilterblok.vue
+++ src/components/filter/LedenlijstFilterblok.vue
@@ -86,8 +86,7 @@
                   @deactivateCriterium="deactivateCriterium"
                   v-if="criteria.criteriaKey === 'adresgeblokkeerd' || criteria.criteriaKey === 'verminderdLidgeld'
                   || criteria.criteriaKey === 'emailgeblokkeerd'    || criteria.criteriaKey === 'emailleeg'
-                  || criteria.criteriaKey === 'geenLidkaart'        || criteria.criteriaKey === 'geweigerdLid'
-                  || criteria.criteriaKey === 'rijksregisternummer'">
+                  || criteria.criteriaKey === 'geenLidkaart'        || criteria.criteriaKey === 'geweigerdLid'">
       </BoolFilter>
       <OudLedenSelect :criteria="criteria" v-if="criteria.criteriaKey === 'oudleden'" :value="criteria.value"
                       @deactivateCriterium="deactivateCriterium"></OudLedenSelect>
