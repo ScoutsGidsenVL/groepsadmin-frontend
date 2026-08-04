2f4095868854728c6155c519142124a0bb10a4d2
Koen De Voegt <kdv@scoutsengisdenvlaanderen.be>
2026-08-04 14:50:43 +0200

    T7382 - update tekst pop-up

--- src/services/dashboard/DashboardService.js
+++ src/services/dashboard/DashboardService.js
@@ -104,18 +104,23 @@ export default {
       if (!profiel) {
         return "";
       }
-      const gsm =
-        (profiel.persoonsgegevens && profiel.persoonsgegevens.gsm) || "-";
+      const gsmRuw = profiel.persoonsgegevens && profiel.persoonsgegevens.gsm;
+      const gsm = gsmRuw || "-";
+      const heeftGeenContactgegevens = !profiel.email && !gsmRuw;
+      const waarschuwing = heeftGeenContactgegevens
+        ? `<p class="welkom-dialog-waarschuwing"><i class="fas fa-exclamation-triangle"></i>Opgelet: er is geen e-mailadres en geen gsm-nummer gekend. Vul minstens één van beide aan.</p>`
+        : "";
       return (
-        `<p>Je bent nu geregistreerd op de Groepsadministratie van Scouts & gidsen Vlaanderen. Hier kan je je <span class="welkom-dialog-nowrap"><i class="far fa-user welkom-dialog-tekst-icoon"></i><span class="welkom-dialog-link">persoonlijke gegevens</span></span> en je
-        <span class="welkom-dialog-nowrap"><i class="fal fa-notes-medical welkom-dialog-tekst-icoon"></i><a href="https://www.scoutsengidsenvlaanderen.be/ouders/praktisch/inschrijven/individuele-steekkaart" target="_blank" class="welkom-dialog-link">individuele steekkaart</a></span> bekijken en aanpassen.</p>` +
+        `<p>Je bent nu geregistreerd op de Groepsadministratie van Scouts & gidsen Vlaanderen. Hier kan je jouw <span class="welkom-dialog-nowrap"><i class="far fa-user welkom-dialog-tekst-icoon"></i><span class="welkom-dialog-link">persoonlijke gegevens</span></span> en
+          <span class="welkom-dialog-nowrap"><i class="fal fa-notes-medical welkom-dialog-tekst-icoon"></i><a href="https://www.scoutsengidsenvlaanderen.be/ouders/praktisch/inschrijven/individuele-steekkaart" target="_blank" class="welkom-dialog-link">individuele steekkaart</a></span> bekijken en aanpassen.</p>` +
         `<p>Je account is aangemaakt met volgende gegevens:</p>` +
         `<ul>` +
         `<li>Gebruikersnaam: <b>${store.getters.gebruikersnaam}</b></li>` +
-        `<li>E-mail: <b>${profiel.email}</b></li>` +
+        `<li>E-mail: <b>${profiel.email || "-"}</b></li>` +
         `<li>GSM: <b>${gsm}</b></li>` +
         `</ul>` +
-        `<p>Controleer zeker je E-mail (en GSM-nummer) zodat we je kunnen bereiken en je je account kan beheren. ` +
+        waarschuwing +
+        `<p>Controleer zeker je E-mail (en GSM-nummer) zodat we je kunnen bereiken en je de toegang tot jouw account niet verliest. ` +
         `Meer informatie vind je <a href="https://scoutsgidsenvl.sharepoint.com/:w:/s/Ledenadministratie-site/IQCKJiayJKfqVZsugQ6YZ-wAAc7w2s7BZppO09b9u9kiS-w" target="_blank">hier</a>.</p>`
       );
     });
